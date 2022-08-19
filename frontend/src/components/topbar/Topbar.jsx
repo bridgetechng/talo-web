001c448bde0f532eac5d884f010f5b6808d3c1c6
Dagogo Clinton Uranta <dagogouranta@gmail.com>
2022-08-19 13:02:12 +0100

    both moved the log out button and added functionality to it

--- frontend/src/components/topbar/Topbar.jsx
+++ frontend/src/components/topbar/Topbar.jsx
@@ -1,18 +1,35 @@
 //i just learnt you can save as a jsx file here!
 
 import React from 'react'
+import { useNavigate} from 'react-router-dom';
 import './topbar.css'
 import logo from '../../images/uhlogo.png'
 import profile from '../../images/sample-profile.jpg'
 import { NotificationsNone,Settings} from '@mui/icons-material/';
+import ExpandCircleDownIcon from '@mui/icons-material/ExpandCircleDown';
 import { useState,useEffect } from 'react'
 import { Button } from 'react-bootstrap';
 
 export default function Topbar(){
 
+    const navigate = useNavigate()
+
+    const [visible,setVisible] = useState(false)
     const [userInfo,setUserInfo]  = useState(JSON.parse(window.sessionStorage.getItem('userInfo')))
 
+    const upMenu = function(){
+        setVisible(true)
+     }
+  
+     const downMenu = function(){
+  
+       setVisible(false)
+     }
 
+     const logOut = function(){
+        sessionStorage.removeItem('userInfo')
+        navigate('/')
+      }
 
     return (
         
@@ -21,7 +38,7 @@ export default function Topbar(){
              
                 <div className="topLeft">
                      <img src={logo} className="uh" alt='urban hive logo'></img>
-                     <span className='logOut'><button className="widgetSmButton1">LOG OUT </button></span>
+                    
                     </div>
 
                          
@@ -37,13 +54,18 @@ export default function Topbar(){
                           {' '}
                           {userInfo.userInfo.lastName.toUpperCase()}
                           &nbsp;&nbsp;
-                            
+                            <ExpandCircleDownIcon style={{backgroundColor:"inherit",color:"rgb(30, 121, 3)"}} onClick={upMenu} onMouseLeave={downMenu}/>
                           </span>
                          
-                         <NotificationsNone className="notificationIcon"/>
+                         <NotificationsNone className="notificationIcon"  />
                          <span className="topIconBadge">
                              2
                          </span>
+
+                       {visible &&  <div className="logOutOptions" onMouseEnter={upMenu} onMouseLeave={downMenu} onClick={logOut}>
+                                     <div className="optionItem1"> <span className='logOut'><button className="widgetSmButton1">LOG OUT </button></span></div>
+                          
+                                 </div>}
                          
 
                       </div>
--- frontend/src/pages/homepage/Homepage.jsx
+++ frontend/src/pages/homepage/Homepage.jsx
@@ -4,6 +4,7 @@ import Grid from '@mui/material/Grid';
 import FilterListIcon from '@mui/icons-material/FilterList';
 import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
 import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
+
 import magGlass from "../../images/search.png"
 
 import "./homepage.css";
